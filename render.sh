9d72b3e947b137d7acd558da07f0fc7348d83134
Jacob Mellin <mail@jacobmellin.de>
2023-07-28 19:45:00 +0200

    Adjust hints and styling

--- render.sh
+++ render.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p out
-for i in {1..7}
+for i in {1..25}
 do
     node html5-animation-video-renderer/render.js --url=http://localhost:1234#h${i} --alpha --video=out/h${i}.mov
 done
