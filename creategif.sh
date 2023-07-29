c929e53e1426e6c995dda917d5c9623a438f2edb
Jacob Mellin <mail@jacobmellin.de>
2023-07-29 12:56:41 +0200

    Update example gif

--- creategif.sh
+++ creategif.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # ffmpeg -i out/h2.mov -vf cropdetect /tmp/out.mov
-ffmpeg -i out/h2.mov -vf "crop=448:400:56:548,fps=30,scale=400:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 ./images/h2_small.gif
+ffmpeg -i out/h2.mov -vf "crop=470:420:50:548,fps=30,scale=400:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 ./images/h2_small.gif
